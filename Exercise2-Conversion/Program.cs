3a0b13f660acd4e5e42728586e7601a6c463e0ce
Angelo Solis <88774848+angelosolis@users.noreply.github.com>
2023-03-04 14:16:11 +0800

    Add files via upload

---
+++ Exercise2-Conversion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2_Conversion
+{
+    internal class Conversion
+    {
+        static void Main()
+        {
+            Console.Write("Enter length (in centimeters): ");
+            string input = Console.ReadLine();
+            int lengthCm = Convert.ToInt32(input);
+            if (lengthCm > 0)
+            {
+                double lengthIn = Math.Round(lengthCm / 2.54);
+
+                int yards = (int)(lengthIn / 36);
+                int feet = (int)((lengthIn % 36) / 12);
+                int inches = (int)(lengthIn % 12);
+
+                Console.WriteLine("{0} yards, {1} feet, {2} inches",yards,feet,inches);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer.");
+            }
+        }
+    }
+
+}
